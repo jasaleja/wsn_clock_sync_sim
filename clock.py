c14b0bef28df37723756984c3b39b4d25177106b
jasaleja <jasaleja@gmail.com>
2023-04-29 21:47:04 +0200

    Adjusted scramble numbers and few renamings done.

--- clock.py
+++ clock.py
@@ -11,20 +11,17 @@ class Clock:
     def __str__(self) -> str:
         return f"{self.time}"
     
-    def get_time(self, systemTick: int) -> float:
-        self.time = self.skew * systemTick + self.time
+    def get_time(self, passed_ticks: int) -> float:
+        self.time = self.skew * passed_ticks + self.time
         return self.time
     
-    def scramlbe_time(self):
-        #Scramble skew rate from 0.9-1.1
-        self.skew = 1 + randint(-10,10)/10
-        #Scramble offset from -10 to 10 seconds
-        self.time = self.time + randint(-10,10)
-
-    def set_system_time(self, systemTick: int) -> None:
-        self.time = systemTick
-        self.skew = 1
+    def scramble_time(self) -> None:
+        #Scramble skew rate from 0.95-1.05
+        self.skew = 1 + randint(-5,5)/100
+        #Scramble offset from -2 to 2 seconds
+        self.time = self.time + randint(-2,2)
 
 if __name__ == "__main__":
+    #Test code for current class
     exampleClock = Clock(0, 1)
     print(exampleClock)
