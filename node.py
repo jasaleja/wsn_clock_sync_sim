b10f43cb4018f6ea86fb48ebb8ae3fd17a42701f
jasaleja <jasaleja@gmail.com>
2023-05-01 21:01:04 +0200

    Confidence was moved to the ccs algorithm. General code refactoring.

--- node.py
+++ node.py
@@ -1,52 +1,55 @@
+"""Mode for class Node."""
 from math import sqrt
 from uuid import uuid1
 from clock import Clock
 
-
 class Node(Clock):
-    def __init__(self, position: tuple[int, int], range: float) -> None:
-        self.id = uuid1()
+    """Class for representation of a node inside a network that has an internal clock."""
+    def __init__(self, position: tuple[int, int], transmission_range: float) -> None:
+        self.id = uuid1() # pylint: disable=invalid-name
         Clock.__init__(self)
         self.position = position
-        self.range = range
+        self.transmission_range = transmission_range
         self.active = True
-        self.confidence = 1
 
     def __repr__(self) -> str:
         return self.__str__()
-    
+
     def __str__(self) -> str:
-        return f"Node \033[31m{self.id}\033[39m at coordinates {self.position} with time {self.time}.\n"
-    
+        return f"Node \033[31m{self.id}\033[39m at coordinates {self.position}.\n"
+
     def move_to(self, new_position: tuple[int, int]) -> None:
+        """Move the node to another position."""
         self.position = new_position
 
     def turn_off(self) -> None:
-        if (self.active == True):
+        "Disable the node."
+        if self.active is True:
             self.active = False
         else:
             print(f"Node \033[31m{self.id}\033[39m is already off.\n")
 
     def turn_on(self) -> None:
-        if (self.active == False):
+        """Activate the node."""
+        if self.active is False:
             self.active = True
         else:
             print(f"Node \033[31m{self.id}\033[39m is already on.\n")
 
     def in_range(self, receiver_position: tuple[float, float], receiver_id: str) -> bool:
-        if (self.id == receiver_id):
-            return False
-        else:
-            x,y = receiver_position
-            distance = sqrt((self.position[0] - x)**2 + (self.position[1]-y)**2)
-            if distance < self.range:
+        """Check if receiver node is in trasmission range of the sender node."""
+        # Check if node is not itself
+        if self.id != receiver_id:
+            x_position, y_position = receiver_position
+            distance = sqrt((self.position[0] - x_position)**2 + (self.position[1]-y_position)**2)
+            # Check distance is in range
+            if distance <= self.transmission_range:
                 return True
-            else:
-                return False
 
-            
+        return False
+
 if __name__ == "__main__":
-    #Test code for current class
+    # Test code for class Node
     exampleNode = Node((0,0), 1)
     exampleNode.turn_on()
     exampleNode2 = Node((1,1), 1)
