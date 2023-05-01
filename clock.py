9126c2addcdaab0608a679a5ccc50000927c0ff8
jasaleja <jasaleja@gmail.com>
2023-05-01 20:30:28 +0200

    Added difference between natural skew and componsation skew. Clock scramble is now done through parameters.

--- clock.py
+++ clock.py
@@ -1,27 +1,33 @@
-from random import randint
+"""Mode for class Clock."""
 
 class Clock:
+    """Class for mathematical representation of a clock."""
     def __init__(self, offset: float = 0, skew:float = 1) -> None:
         self.time = offset
-        self.skew = skew
+        # Unknow to the user and should not be edited once set
+        self.natural_skew = skew
+        # Known to the user
+        self.compensation_skew = 1
 
     def __repr__(self) -> str:
         return self.__str__()
-    
+
     def __str__(self) -> str:
         return f"{self.time}"
-    
-    def get_time(self, passed_ticks: int) -> float:
-        self.time = self.skew * passed_ticks + self.time
+
+    def pass_time(self, passed_seconds: int) -> float:
+        """Advance the clock by the amount of passed seconds."""
+        self.time = (self.natural_skew/self.compensation_skew) * passed_seconds + self.time
         return self.time
-    
-    def scramble_time(self) -> None:
-        #Scramble skew rate from 0.95-1.05
-        self.skew = 1 + randint(-5,5)/100
-        #Scramble offset from -2 to 2 seconds
-        self.time = self.time + randint(-2,2)
+
+    def scramble_time(self, skew: float, offset: float) -> None:
+        """Introduce an error in the skew rate and offset."""
+        #Scramble skew rate
+        self.natural_skew = 1 + skew
+        #Scramble offset
+        self.time = self.time + offset
 
 if __name__ == "__main__":
-    #Test code for current class
+    # Test code for class Clock
     exampleClock = Clock(0, 1)
     print(exampleClock)
