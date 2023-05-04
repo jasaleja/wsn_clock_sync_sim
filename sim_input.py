0337107f363b6b84b00d8ac992ea399ee86c80ab
jasaleja <jasaleja@gmail.com>
2023-05-04 07:40:37 +0200

    Now it is not important in what order the data is in the file.

--- sim_input.py
+++ sim_input.py
@@ -16,7 +16,8 @@ def parse_input(file):
     """
     Parse input file and return it as a set.
     """
-    parsed_data = []
+    parsed_data = ["random", (10, 10), 30, 5, 0.0005, 3000]
+    print(parsed_data)
     # Open the file and read through it line by line
     with open(file, 'r', encoding='utf-8') as file_object:
         line = file_object.readline()
@@ -26,18 +27,18 @@ def parse_input(file):
                 if found:
                     match key:
                         case 'topology':
-                            parsed_data.append(found.group(key))
+                            parsed_data[0] = found.group(key)
                         case 'size':
                             dimensions = found.group(key).split('x')
-                            parsed_data.append([int(dimensions[0]), int(dimensions[1])])
+                            parsed_data[1] = (int(dimensions[0]), int(dimensions[1]))
                         case 'node_amount':
-                            parsed_data.append(int(found.group(key)))
+                            parsed_data[2] = int(found.group(key))
                         case 'transmission_range':
-                            parsed_data.append(int(found.group(key)))
+                            parsed_data[3] = int(found.group(key))
                         case 'natural_skew':
-                            parsed_data.append(float(found.group(key)))
+                            parsed_data[4] = float(found.group(key))
                         case 'initial_offset':
-                            parsed_data.append(int(found.group(key)))
+                            parsed_data[5] = int(found.group(key))
                         case _:
                             pass
 
